c6ba26b69fe1070109b2f6aeb6714c04d11cbc7e
sriram1998 <xmenram@gmail.com>
2017-06-04 08:33:58 +0530

    hacker mode

--- app/src/main/java/com/example/sriram/colorapp/MainActivity.java
+++ app/src/main/java/com/example/sriram/colorapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sriram.colorapp;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,43 +12,76 @@ public class MainActivity extends AppCompatActivity {
     int i=0;
     int j=0;
     int k=0;
+
+
+
+
     TextView textView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {SharedPreferences sharedPref=getSharedPreferences("bginfo",0);
+
+        int ir,jr,kr;
+        ir=sharedPref.getInt("bgr",0);
+        view=this.findViewById(android.R.id.content);
+        jr=sharedPref.getInt("bgg",0);
+        kr=sharedPref.getInt("bgb",0);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        view=this.getWindow().getDecorView();
-        view.setBackgroundColor(Color.rgb(0,0,0));
+
+        view.setBackgroundColor(Color.rgb(ir,jr,kr));
         textView=(TextView) findViewById(R.id.textView);
     }
 
+
     public void chred(View v) {
+        SharedPreferences sharedPref=getSharedPreferences("bginfo",0);
+        SharedPreferences.Editor editor=sharedPref.edit();
+        i=sharedPref.getInt("bgr",0);
+        j=sharedPref.getInt("bgg",0);
+        k=sharedPref.getInt("bgb",0);
         if (i == 256)
             i = 0;
         else {
             view.setBackgroundColor(Color.rgb(i, j, k));
             i=i+1;
             textView.setText("red:"+String.valueOf(i)+"   green:"+String.valueOf(j)+"  blue:"+String.valueOf(k));
+            editor.putInt("bgr",i);
+            editor.apply();
+
         }
     }
 
     public void chgreen(View v)
-    {if (j == 256)
+    {SharedPreferences sharedPref=getSharedPreferences("bginfo",0);
+        SharedPreferences.Editor editor=sharedPref.edit();
+        i=sharedPref.getInt("bgr",0);
+        j=sharedPref.getInt("bgg",0);
+        k=sharedPref.getInt("bgb",0);
+        if (j == 256)
         j = 0;
     else {
         view.setBackgroundColor(Color.rgb(i, j, k));
         j=j+1;
         textView.setText("red:"+String.valueOf(i)+"   green:"+String.valueOf(j)+"  blue:"+String.valueOf(k));
+        editor.putInt("bgg",j);
+        editor.apply();
     }}
 
 
     public void chblue(View v)
-    {if (k == 256)
+    {SharedPreferences sharedPref=getSharedPreferences("bginfo",0);
+        SharedPreferences.Editor editor=sharedPref.edit();
+        i=sharedPref.getInt("bgr",0);
+        j=sharedPref.getInt("bgg",0);
+        k=sharedPref.getInt("bgb",0);
+        if (k == 256)
         k = 0;
     else {
         view.setBackgroundColor(Color.rgb(i, j, k));
         k=k+1;
         textView.setText("red:"+String.valueOf(i)+"   green:"+String.valueOf(j)+"  blue:"+String.valueOf(k));
+        editor.putInt("bgb",k);
+        editor.apply();
     }
     }
 
