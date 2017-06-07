81406ea82ad7773c4b8c835cccf4e6a0bba583d0
sriram1998 <xmenram@gmail.com>
2017-06-07 23:54:12 +0530

    seekbar done

--- app/src/main/java/com/example/sriram/colorapp/MainActivity.java
+++ app/src/main/java/com/example/sriram/colorapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,27 +13,100 @@ public class MainActivity extends AppCompatActivity {
     int i=0;
     int j=0;
     int k=0;
-
-
-
+    SeekBar seekbar1;
+    SeekBar seekbar2;
+    SeekBar seekbar3;
 
     TextView textView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {SharedPreferences sharedPref=getSharedPreferences("bginfo",0);
-
-        int ir,jr,kr;
-        ir=sharedPref.getInt("bgr",0);
-        view=this.findViewById(android.R.id.content);
-        jr=sharedPref.getInt("bgg",0);
-        kr=sharedPref.getInt("bgb",0);
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences sharedPref=getSharedPreferences("bginfo",0);
+        view=this.findViewById(android.R.id.content);
         setContentView(R.layout.activity_main);
 
+        seekbar1=(SeekBar) findViewById(R.id.seekBar);
+        seekbar2=(SeekBar) findViewById(R.id.seekBar2);
+        seekbar3=(SeekBar) findViewById(R.id.seekBar3);
+
+        seekbar1.setMax(255);
+        seekbar2.setMax(255);
+        seekbar3.setMax(255);
+        int ir,jr,kr;
+        ir=sharedPref.getInt("bgr",0);
+
+        jr=sharedPref.getInt("bgg",0);
+        kr=sharedPref.getInt("bgb",0);
         view.setBackgroundColor(Color.rgb(ir,jr,kr));
+        seekbar1.setProgress(ir);
+        seekbar2.setProgress(jr);
+        seekbar3.setProgress(kr);
+
+
         textView=(TextView) findViewById(R.id.textView);
-        textView.setTextColor(Color.rgb(100,100,100));
+        textView.setTextColor(Color.rgb(230,200,200));
         textView.setText("red:"+String.valueOf(ir)+"   green:"+String.valueOf(jr)+"  blue:"+String.valueOf(kr));
+        seekbar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                i=progress;
+                chred(view);
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekbar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                j=progress;
+                chgreen(view);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekbar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                k=progress;
+                chblue(view);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
     }
+
+
 
     public void reset(View v){
         SharedPreferences sharedPref=getSharedPreferences("bginfo",0);
@@ -40,9 +114,14 @@ public class MainActivity extends AppCompatActivity {
         i=0;
         j=0;
         k=0;
-
         view.setBackgroundColor(Color.rgb(i, j, k));
         textView.setText("red:"+String.valueOf(i)+"   green:"+String.valueOf(j)+"  blue:"+String.valueOf(k));
+        seekbar1.setProgress(0);
+        seekbar2.setProgress(0);
+        seekbar3.setProgress(0);
+
+
+
         editor.putInt("bgr",i);
         editor.putInt("bgg",j);
         editor.putInt("bgb",k);
@@ -52,15 +131,11 @@ public class MainActivity extends AppCompatActivity {
     public void chred(View v) {
         SharedPreferences sharedPref=getSharedPreferences("bginfo",0);
         SharedPreferences.Editor editor=sharedPref.edit();
+
         i=sharedPref.getInt("bgr",0);
         j=sharedPref.getInt("bgg",0);
         k=sharedPref.getInt("bgb",0);
-        if (i == 256)
-            i = 0;
-        else {
-
-            i=i+1;
-        }
+        i=seekbar1.getProgress();
         view.setBackgroundColor(Color.rgb(i, j, k));
         textView.setText("red:"+String.valueOf(i)+"   green:"+String.valueOf(j)+"  blue:"+String.valueOf(k));
         editor.putInt("bgr",i);
@@ -70,17 +145,13 @@ public class MainActivity extends AppCompatActivity {
     public void chgreen(View v)
     {SharedPreferences sharedPref=getSharedPreferences("bginfo",0);
         SharedPreferences.Editor editor=sharedPref.edit();
+
         i=sharedPref.getInt("bgr",0);
         j=sharedPref.getInt("bgg",0);
         k=sharedPref.getInt("bgb",0);
-        if (j == 256)
-        j = 0;
-    else {
 
-        j=j+1;
-
-    }
-        view.setBackgroundColor(Color.rgb(i, j, k));
+        j=seekbar2.getProgress();
+       view.setBackgroundColor(Color.rgb(i, j, k));
         textView.setText("red:"+String.valueOf(i)+"   green:"+String.valueOf(j)+"  blue:"+String.valueOf(k));
         editor.putInt("bgg",j);
         editor.apply();
@@ -90,16 +161,12 @@ public class MainActivity extends AppCompatActivity {
     public void chblue(View v)
     {SharedPreferences sharedPref=getSharedPreferences("bginfo",0);
         SharedPreferences.Editor editor=sharedPref.edit();
+
         i=sharedPref.getInt("bgr",0);
         j=sharedPref.getInt("bgg",0);
         k=sharedPref.getInt("bgb",0);
-        if (k == 256)
-        k = 0;
-    else {
 
-        k=k+1;
-
-    }
+        k=seekbar3.getProgress();
         view.setBackgroundColor(Color.rgb(i, j, k));
         textView.setText("red:"+String.valueOf(i)+"   green:"+String.valueOf(j)+"  blue:"+String.valueOf(k));
         editor.putInt("bgb",k);
